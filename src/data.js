d432e75959bfb305c98f6e8f7c5338f235b38876
ainiazzah <aini.21006@mhs.unesa.ac.id>
2025-01-09 12:13:06 +0700

    add: contact data

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+        {
+            id: 1, 
+            name: 'David Septian',
+            tag: 'davidsp',
+            imageUrl: '/images/default.jpg',
+        },
+        {
+            id: 2,
+            name: 'Arifin',
+            tag: 'arifin',
+            imageUrl: '/images/arif.jpeg',
+        },
+        {
+            id: 3,
+            name: 'Rizky Pratama',
+            tag: 'rpratama18',
+            imageUrl: '/images/default.jpg',
+        },
+    ];
+}
+
+export {getData};
